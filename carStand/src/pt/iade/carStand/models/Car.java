8c016f04febd39d2798e903478de836a71f57df2
Diogo Sousa <becadicasousa@gmail.com>
2019-11-22 16:29:53 +0000

    Novos models

--- carStand/src/pt/iade/carStand/models/Car.java
+++ carStand/src/pt/iade/carStand/models/Car.java
@@ -1,9 +1,51 @@
 package pt.iade.carStand.models;
 
-import pt.iade.carStand.models.Item;
-
-public class Car {
-	private double quantity;
+public class Car {	
+	private String marca;
+	private String modelo;
+	private String cor;
+	private int cilindrada;
+	private int ano;
+	private int preco;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public int getCilindrada() {
+		return cilindrada;
+	}
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getPreco() {
+		return preco;
+	}
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+	
+	/*private double quantity;
 	private Item product;
 	public Car(double quantity, Item product) {
 		this.quantity = quantity;
@@ -22,6 +64,6 @@ public class Car {
 	public String toString() {
 		return quantity + " X " + product + " - Total : "+
 				String.format("%.2f", getCost())+" €";
-	}
+	} */
 	
 }
