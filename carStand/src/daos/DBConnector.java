53df1a91fccad6b0003537b6e372ef97194435b3
Diogo Sousa <becadicasousa@gmail.com>
2019-12-13 15:55:57 +0000

    Atualização DataBase

--- carStand/src/daos/DBConnector.java
+++ carStand/src/daos/DBConnector.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class DBConnector {
-	private static final String URL ="jdbc:mysql://remotemysql.com:3306/54tXwN8Pvu?useSSL=false";
-	private static final String PASS ="76xzEVYYOF";
-	private static final String USER ="54tXwN8Pvu";
+	private static final String URL ="jdbc:mysql://remotemysql.com:3306/b01PMoXIWq?useSSL=false";
+	private static final String PASS ="peKOLRcmOX";
+	private static final String USER ="b01PMoXIWq";
 
 	private static Connection connector;
 	private DBConnector () {
